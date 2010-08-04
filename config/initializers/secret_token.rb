d192c3ffe04c69a24bb65bc212a8d4507346a2e4
Paul Wilson <paulanthonywilson@gmail.com>
2010-08-04 17:58:39 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enigma::Application.config.secret_token = 'da053d458f52467859a227552c08430b8a091ae66812b54c07a4b5fe355e75199e336c36629aa16226203ce5e6c676d28b87012fcb4ddd2846804b77e704bc29'
