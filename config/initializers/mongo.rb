14d58ca10ef88a2dc0092b3d04a5e1ecff952751
Paul Wilson <paulanthonywilson@gmail.com>
2010-08-06 23:21:59 +0100

    attempt heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,10 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "enigma-#{Rails.env}"
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(RAILS_ENV)
+else
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+  MongoMapper.database = "enigma-#{Rails.env}"
+end
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
