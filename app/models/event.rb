b8cc1d95c6d75b7dc00cbfe7d6fb7ccb127cc200
Paul Wilson <paulanthonywilson@gmail.com>
2010-08-10 17:59:18 +0100

    Expenses added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,23 @@
+
+
 class Event
   include MongoMapper::Document
 
-  key :name
+  key :name, String, :required=>true
+
+  many :fixed_expenses
+
+end
+
+
+class Expense
+  include MongoMapper::EmbeddedDocument
+
+  key :amount, BigDecimal, :required=>true
+  key :description, String, :required=>true
+  belongs_to :event
+end
+
+class FixedExpense < Expense
+  
 end
