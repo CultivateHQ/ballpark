1b14996f9299e05b1a077382c4f88c14568998fd
Paul Wilson <paulanthonywilson@gmail.com>
2010-08-30 11:52:49 +0100

    Add delete and edit links for expense

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def jquery_icon(icon, title)
+    "<div class='ui-state-default ui-corner-all'><span class='ui-icon ui-icon-#{icon}' title='#{title}'>${title}</span></div>"
+  end
 end
