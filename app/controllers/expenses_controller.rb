0b6d23e72b0f85674465a8fe9dc4922ff083899c
Paul Wilson <paulanthonywilson@gmail.com>
2010-08-30 16:13:36 +0100

    Use template methods for expenses in controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -6,25 +6,25 @@ class ExpensesController < ApplicationController
   def create
     @expense = Expense.new(params[:expense])
     if @expense.valid?
-      @event.fixed_expenses << @expense
+      expenses << @expense
       @event.save!
-      redirect_to event_expenses_path(@event.id)
+      redirect_to expenses_path
     else
       render :index
     end
   end
 
   def destroy
-    @event.fixed_expenses.delete @expense
+    expenses.delete @expense
     @event.save!
-    redirect_to event_expenses_path(@event.id)
+    redirect_to expenses_path
   end
 
   def update
     @expense.assign params[:expense]
     if @expense.valid?
       @event.save
-      redirect_to event_expenses_path(@event.id)
+      redirect_to expenses_path
     else
       render :edit
     end
@@ -38,7 +38,7 @@ class ExpensesController < ApplicationController
 
   def find_event_and_expense
     @event = Event.find(params[:event_id])
-    @expense = @event.fixed_expenses.find(params[:id]) if params[:id]
+    @expense = expenses.find(params[:id]) if params[:id]
   end
 
   def expenses_title
