14cab85cb1233114766455f25236ea4715625dcd
Paul Wilson <paulanthonywilson@gmail.com>
2010-11-20 15:18:56 +0000

    basic authentication hard coded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_filter :ensure_ssl_if_production_and_heroku
+  before_filter :verify_access
   protect_from_forgery
 
   def find_event
@@ -10,6 +11,13 @@ class ApplicationController < ActionController::Base
 
   def ensure_ssl_if_production_and_heroku
     redirect_to "https://#{request.host}#{request.path_info}" if "production" == RAILS_ENV && 'https' != request.env['HTTP_X_FORWARDED_PROTO']
+  end
+
+
+  def verify_access
+    authenticate_or_request_with_http_basic("enigma") do |username, password|
+      username == "ruby" && password == "w3w01f"
+    end
   end
 
 end
