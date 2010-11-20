c1ae5de13f6334d9d7c28c77535ecd7c8ee4366e
Paul Wilson <paulanthonywilson@gmail.com>
2010-11-20 15:09:46 +0000

    try ensuring ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :ensure_ssl_if_production_and_heroku
   protect_from_forgery
 
   def find_event
     @event = Event.find(params[:event_id])
+  end
+
+  private
+
+  def ensure_ssl_if_production_and_heroku
+    redirect_to "https://#{request.host}#{request.path_info}" if "production" == RAILS_ENV && 'https' != request.env['HTTP_X_FORWARDED_PROTO']
   end
 
 end
